ada09ce0fdfba38be312610165b9386058e7ec7e
Ahmadullah Masoudy <ehabahmed@Ahmadullah-MacBook-Pro.local>
2024-05-12 04:07:52 +0300

    update link in contants

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -10,7 +10,7 @@ export const MapUrl = [
   {
     title: "Topo 1",
     key: "topo1",
-    href: "http://localhost:8000/maps/topographical-map.php?start=50.444434&-34.343434&end=34.4446&-44.343434",
+    href: "http://localhost:9000/maps/topographical-map.php",
     description: "This Route is for Practice and you can get In to it.",
     streets: "Vauxhall to birmingam",
   },
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,10 +73,10 @@ const config = {
           to: { height: "0" },
         },
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      // animation: {
+      //   "accordion-down": "accordion-down 0.2s ease-out",
+      //   "accordion-up": "accordion-up 0.2s ease-out",
+      // },
     },
   },
   plugins: [require("tailwindcss-animate")],
