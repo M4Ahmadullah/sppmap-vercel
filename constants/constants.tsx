4cf1b33968f9fa2ce7610b90fae78e72a6359eda
Ahmadullah Masoudy <ehabahmed@Ahmadullah-MacBook-Pro.local>
2024-05-16 06:56:01 +0300

    new cons

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -10,7 +10,7 @@ export const MapUrl = [
   {
     title: "Topo 1",
     key: "topo1",
-    href: "http://localhost:9000/maps/topographical-map.php",
+    href: "http://sppmap.com/map.php?startX=20&startY=20&endX=22&endY=21",
     description: "This Route is for Practice and you can get In to it.",
     streets: "Vauxhall to birmingam",
   },
