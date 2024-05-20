1ccdc763bf50e4ef62ab9ad44584d3ba383c5d96
Ahmadullah Masoudy <ehabahmed@Ahmadullah-MacBook-Pro.local>
2024-05-20 18:05:30 +0300

    updating.

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -17,7 +17,7 @@ export const MapUrl = [
   {
     title: "Topo 1",
     key: "topo1",
-    href: "http://sppmap.com/map.php?startX=20&startY=20&endX=22&endY=21",
+    href: "/app/topo/route1",
     description: "This Route is for Practice and you can get In to it.",
     streets: "Vauxhall to birmingam",
   },
