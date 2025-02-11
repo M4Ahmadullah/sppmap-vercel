e227ec1ea87ce23fe49bd06ec799644a8985b562
Ahmadullah Masoudy <ehabahmed@AMs-MacBook-Pro.local>
2025-02-11 21:34:54 +0300

    Updated project with new changes

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -36,3 +36,43 @@ export const MapUrl = [
     streets: "Vauxhall to birmingam",
   },
 ];
+
+// Previous map routes
+// https://sppmap.vercel.app/prevtopo/routes/keepingleft1
+// https://sppmap.vercel.app/prevtopo/routes/onwayroad1
+// https://sppmap.vercel.app/prevtopo/routes/railwaylines1
+// https://sppmap.vercel.app/prevtopo/routes/roundabouts1
+// https://sppmap.vercel.app/prevtopo/routes/footbridges1
+// https://sppmap.vercel.app/prevtopo/routes/footpaths1
+// https://sppmap.vercel.app/prevtopo/routes/deadends1
+// https://sppmap.vercel.app/prevtopo/routes/restrictedaccess1
+// https://sppmap.vercel.app/prevtopo/routes/pedestrianzone1
+// https://sppmap.vercel.app/prevtopo/routes/centralreservation1
+// https://sppmap.vercel.app/prevtopo/routes/roadsontop1
+// https://sppmap.vercel.app/prevtopo/routes/flyoverroundabouts1
+// https://sppmap.vercel.app/prevtopo/routes/unmarkedoneways1
+// https://sppmap.vercel.app/prevtopo/routes/cityroutes1
+// https://sppmap.vercel.app/prevtopo/routes/shortroutes1
+// https://sppmap.vercel.app/prevtopo/routes/closedroads1
+// https://sppmap.vercel.app/prevtopo/routes/tunnels1
+// https://sppmap.vercel.app/prevtopo/routes/otherroutes1
+
+// New Routes of the map
+// https://sppmap.vercel.app/newtopo/routes/keepingleft1
+// https://sppmap.vercel.app/newtopo/routes/onwayroad1
+// https://sppmap.vercel.app/newtopo/routes/railwaylines1
+// https://sppmap.vercel.app/newtopo/routes/roundabouts1
+// https://sppmap.vercel.app/newtopo/routes/footbridges1
+// https://sppmap.vercel.app/newtopo/routes/footpaths1
+// https://sppmap.vercel.app/newtopo/routes/deadends1
+// https://sppmap.vercel.app/newtopo/routes/restrictedaccess1
+// https://sppmap.vercel.app/newtopo/routes/pedestrianzone1
+// https://sppmap.vercel.app/newtopo/routes/centralreservation1
+// https://sppmap.vercel.app/newtopo/routes/roadsontop1
+// https://sppmap.vercel.app/newtopo/routes/flyoverroundabouts1
+// https://sppmap.vercel.app/newtopo/routes/unmarkedoneways1
+// https://sppmap.vercel.app/newtopo/routes/cityroutes1
+// https://sppmap.vercel.app/newtopo/routes/shortroutes1
+// https://sppmap.vercel.app/newtopo/routes/closedroads1
+// https://sppmap.vercel.app/newtopo/routes/tunnels1
+// https://sppmap.vercel.app/newtopo/routes/otherroutes1
--- constants/newroutesurls/iframeURLsMain.tsx
+++ constants/newroutesurls/iframeURLsMain.tsx
@@ -195,28 +195,27 @@ export const iframeURLs = [
 // Tunnels
 export const TunnelsURLs = [
   {
-    title: "Tunnels = [Rotherhite North] > Corbets St -> Brodlove St",
+    title: "R3 [Rotherhithe North] Corbetts Lane SE16 > Brodlove Lane E1",
     href: "https://softmaps.io/map.php?startX=13.93&startY=1.05&endX=15.36&endY=6.08",
   },
   {
-    title: "Tunnels = [Rotherhite South] >  Wapping High St -> Credon Rd",
+    title: "R6 [Rotherhithe South] Wapping High Street E1W > Credon Road SE16",
     href: "https://softmaps.io/map.php?startX=12.55&startY=4.46&endX=12.54&endY=0.466",
   },
   {
-    title:
-      "Tunnels = [Blackwall North] Katherine Close >  Wapping High St, School Cl -> New Vilage St",
+    title: "R5 [Blackwall North] Katherine Close SE16 > Brodlove Lane E1",
     href: "https://softmaps.io/map.php?startX=29.40&startY=2.29&endX=26.95&endY=6.68",
   },
   {
-    title: "Tunnels = [Blackwall South] > Baldock St -> Mauritius St",
+    title: "R7 [Blackwall South] Baldock Street E3 > Mauritius Road SE10",
     href: "https://softmaps.io/map.php?startX=22.97&startY=10.42&endX=29.105&endY=0.88",
   },
   {
-    title: "[Limehouse Link] Jardine Road E1W > Newport Avenue E14",
+    title: "R8 [Limehouse Link] Jardine Road E1W > Newport Avenue E14",
     href: "https://softmaps.io/map.php?startX=16.53&startY=5.97&endX=27.55&endY=5.66",
   },
   {
-    title: "[Dartford Tunnel] Cavell Crescent DA1 > Mayflower Road RM16",
+    title: "R54 [Dartford Tunnel] Cavell Crescent DA1 > Mayflower Road RM16",
     href: "https://softmaps.io/map.php?startX=88.05&startY=-8.83&endX=102.36&endY=-0.75",
   },
 ];
@@ -224,23 +223,25 @@ export const TunnelsURLs = [
 // Closed Routes
 export const ClosedRoadsURLs = [
   {
-    title: "[Cassland Road Closed] / Martello St -> Gascoyne Rd",
+    title: "R17 [Cassland Road Closed] Martello Street E8 > Gascoyne Road E9",
     href: "https://softmaps.io/map.php?startX=12.66&startY=12.7&endX=17.08&endY=12.685",
   },
   {
-    title: "[Glenthorne Road Closed] / NScourt Rd -> Greyhound St",
+    title:
+      "R19 [Glenthorne Road Closed] Ravenscourt Road W6 > Greyhound Road W6",
     href: "https://softmaps.io/map.php?startX=-33.02&startY=2.569&endX=-28.04&endY=-0.1",
   },
   {
-    title: "[Manor Park Road Closed] / Craven Park -> Tubbs Rd",
+    title: "R22 [Manor Park Road Closed] Craven Park NW10 > Tubbs Road NW10",
     href: "https://softmaps.io/map.php?startX=-38.58&startY=12.57&endX=-35.09&endY=10.93",
   },
   {
-    title: "[Osborn Road Closed] / Virginia St -> Spital St",
+    title: "R23 [Osborn Road Closed] Virginia Street E1W > Spital Street E1",
     href: "https://softmaps.io/map.php?startX=10.75&startY=5.73&endX=9.63&endY=8.55",
   },
   {
-    title: "[Vauxhall Road Closed] / The Elm Park Rd -> New Burn Rd",
+    title:
+      "R51 [Vauxhall Bridge Road Closed] Elm Park Road SW3 > Newburn Street SE11",
     href: "https://softmaps.io/map.php?startX=-17.62&startY=0.2&endX=-2.14&endY=0.67",
   },
 ]; // 52
@@ -248,52 +249,54 @@ export const ClosedRoadsURLs = [
 // Other Routes
 export const OtherRoutes = [
   {
-    title: "Formosa Street / Formosa St -> Pelham St",
+    title: "R39 Formosa Street W9 > Pelham Street SW7",
     href: "https://softmaps.io/map.php?startX=-20.12&startY=9.07&endX=-16.89&endY=2.095",
   },
   {
-    title: "Parkway Rd / Parkway Road -> Bramley",
+    title: "R41 Parkway NW1 > Bramley Road W10",
     href: "https://softmaps.io/map.php?startX=-9.38&startY=11.98&endX=-28.15&endY=7.43",
   },
   {
-    title: "[Cassland Road Closed] / Martello St -> Gascoyne Rd",
+    title: "R13 Philbeach Gardens SW5 > Norroy Road SW15",
     href: "https://softmaps.io/map.php?startX=-23.66&startY=1.35&endX=-28.97&endY=-6.494",
   },
   {
-    title: "[Cassland Road Closed] / Martello St -> Gascoyne Rd",
+    title: "R44 Capel Road > Maple Grove WD17",
     href: "https://softmaps.io/map.php?startX=-70.51&startY=26.74&endX=-77.08&endY=28.552",
   },
 ];
 
 export const ShortRoutes = [
   {
-    title: "Barking / Stamford Rd -> Olave's Rd",
+    title: "R9 Stamford Road E6 > St Olave's Road E6",
     href: "https://softmaps.io/map.php?startX=57.74&startY=11.565&endX=43.88&endY=11.195",
   },
   {
-    title: "Hannah Close / Hannah Close St -> Claremont St",
-    href: "https://softmaps.io/map.php?startX=-40.3&startY=15.55&endX=-28.47&endY=17.33",
-  },
-  {
-    title: "Princelet Street / Princelet St -> Tillman St",
+    title: "R38 Princelet Street E1 > Chapman Street E1",
     href: "https://softmaps.io/map.php?startX=9.45&startY=8.159&endX=12.48&endY=6.55",
   },
   {
-    title: "Knox St / Know St -> Rowngtn Cl",
+    title: "R40 Hannah Close NW10 > Claremont Road NW2",
+    href: "https://softmaps.io/map.php?startX=-40.3&startY=15.55&endX=-28.47&endY=17.33",
+  },
+  {
+    title: "R42 Knox Street W1H > Rowington Close W2",
     href: "https://softmaps.io/map.php?startX=-13.76&startY=8.27&endX=-20.85&endY=8.67",
   },
   {
-    title: "Talgarth Rd / Talgarth Rd -> Greyhound St",
+    title: "R45 Talgarth Road W14 > Greyhound Road W6",
     href: "https://softmaps.io/map.php?startX=-25.54&startY=1.285&endX=-28.06&endY=-0.1",
   },
   {
-    title: "Blacksmiths / Blacksmiths Lane -> Florence St",
+    title: "R50 Blacksmith's Lane UB9 > Florence Way UB8",
     href: "https://softmaps.io/map.php?startX=-104.09&startY=17.253&endX=-97.2&endY=13.345",
   },
   {
-    title: "Blackbird Hill / Saxon St -> Pitfield St",
+    title: "R53 Saxon Road HA9 > Meadow Garth NW10",
     href: "https://softmaps.io/map.php?startX=-41.25&startY=16.39&endX=-40.05&endY=13.6",
   },
+
+  // Other Short Routes that don't exist is the Excel Sheet.
   {
     title: "Groove Street -> Beulah Street ",
     href: "https://softmaps.io/map.php?startX=31.185&startY=21.043&endX=25.1&endY=19.24",
@@ -306,15 +309,208 @@ export const ShortRoutes = [
 
 export const CityRoutes = [
   {
-    title: "SOHO / Wardour St -> Austin St",
+    title: "R29 Wardour Street W1F > Austin Friars EC2N",
     href: "https://softmaps.io/map.php?startX=-6.69&startY=6.64&endX=5.72&endY=7.24",
   },
   {
-    title: "Russell Square / Munton Road -> Russell Square Rd",
+    title: "R47 Munton Road SE17 > Russell Square WC1B",
     href: "https://softmaps.io/map.php?startX=3.8&startY=2&endX=-5&endY=8.45",
   },
   {
-    title: "Concert Hall / Bow St -> Concert Hall St",
+    title: "R52 Bow Street WC2E > Concert Hall Approach SE1",
     href: "https://softmaps.io/map.php?startX=-3.6&startY=6.64&endX=-2.01&endY=4.79",
   },
 ]; // 68
+
+export const MockTest1 = [
+  {
+    title: "R6 [Rotherhithe South] Wapping High Street E1W > Credon Road SE16",
+    href: "https://softmaps.io/map.php?startX=12.55&startY=4.46&endX=12.54&endY=0.466",
+  },
+  {
+    title: "R17 [Cassland Road Closed] Martello Street E8 > Gascoyne Road E9",
+    href: "https://softmaps.io/map.php?startX=12.66&startY=12.7&endX=17.08&endY=12.685",
+  },
+  {
+    title: "R39 Formosa Street W9 > Pelham Street SW7",
+    href: "https://softmaps.io/map.php?startX=-20.12&startY=9.07&endX=-16.89&endY=2.095",
+  },
+  {
+    title: "R52 Bow Street WC2E > Concert Hall Approach SE1",
+    href: "https://softmaps.io/map.php?startX=-3.6&startY=6.64&endX=-2.01&endY=4.79",
+  },
+]; // 68
+
+export const MockTest2 = [
+  {
+    title: "R7 [Blackwall South] Baldock Street E3 > Mauritius Road SE10",
+    href: "https://softmaps.io/map.php?startX=22.97&startY=10.42&endX=29.105&endY=0.88",
+  },
+  {
+    title:
+      "R19 [Glenthorne Road Closed] Ravenscourt Road W6 > Greyhound Road W6",
+    href: "https://softmaps.io/map.php?startX=-33.02&startY=2.569&endX=-28.04&endY=-0.1",
+  },
+  {
+    title: "R13 Philbeach Gardens SW5 > Norroy Road SW15",
+    href: "https://softmaps.io/map.php?startX=-23.66&startY=1.35&endX=-28.97&endY=-6.494",
+  },
+  {
+    title: "R53 Saxon Road HA9 > Meadow Garth NW10",
+    href: "https://softmaps.io/map.php?startX=-41.25&startY=16.39&endX=-40.05&endY=13.6",
+  },
+]; // 68
+
+export const MockTest3 = [
+  // R48 is New
+  {
+    title: "R48 Roberts Close SE16 > Hillard's Court E1W",
+    href: "https://softmaps.io/map.php?startX=13.331&startY=4.955&endX=16.65&endY=3.37",
+  },
+  {
+    title: "R22 [Manor Park Road Closed] Craven Park NW10 > Tubbs Road NW10",
+    href: "https://softmaps.io/map.php?startX=-38.58&startY=12.57&endX=-35.09&endY=10.93",
+  },
+  {
+    title: "R44 Capel Road > Maple Grove WD17",
+    href: "https://softmaps.io/map.php?startX=-70.51&startY=26.74&endX=-77.08&endY=28.552",
+  },
+  {
+    title: "R9 Stamford Road E6 > St Olave's Road E6",
+    href: "https://softmaps.io/map.php?startX=57.74&startY=11.565&endX=43.88&endY=11.195",
+  },
+]; // 68
+
+export const MockTest4 = [
+  {
+    title: "R1 Station Approach NW10 > Blackmore Drive NW10",
+    href: "https://softmaps.io/map.php?startX=-35.165&startY=10.725&endX=-42.67&endY=12.675",
+  },
+  {
+    title: "R18 Armoury Way SW18 > Spencer Park SW18",
+    href: "https://softmaps.io/map.php?startX=-22.54&startY=-7.72&endX=-18.1&endY=-8.08",
+  },
+  {
+    title: "R2 Prospect Place E1 > Katherine Close SE16",
+    href: "https://softmaps.io/map.php?startX=13.75&startY=5.174&endX=15.44&endY=4.57",
+  },
+  {
+    title: "R46 Baker Street EN5 > Cowley Hill WD6",
+    href: "https://softmaps.io/map.php?startX=-25.55&startY=29.83&endX=-41.02&endY=28.664",
+  },
+]; // 68
+
+export const FurtherRoutes = [
+  {
+    title: "R1 Station Approach NW10 > Blackmore Drive NW10",
+    href: "https://softmaps.io/map.php?startX=-35.165&startY=10.725&endX=-42.67&endY=12.675",
+  },
+  {
+    title: "R2 Prospect Place E1 > Katherine Close SE16",
+    href: "https://softmaps.io/map.php?startX=13.75&startY=5.174&endX=15.44&endY=4.57",
+  },
+  {
+    title: "R4 North Quay Place E14 > Canon Beck Road SE16",
+    href: "https://softmaps.io/map.php?startX=22.32&startY=5.345&endX=15.155&endY=3.751",
+  },
+  {
+    title: "R10 Calderwood Street SE18 > Belvedere Road SE1",
+    href: "https://softmaps.io/map.php?startX=44.03&startY=1.3&endX=-2.45&endY=4.584",
+  },
+  {
+    title: "R11 East Smithfiled EC1A > Oldhill Street N16",
+    href: "https://softmaps.io/map.php?startX=9.83&startY=5.62&endX=10.77&endY=17.343",
+  }, // 5
+  {
+    title: "R12 Keeley Street WC2B > Westbourne Terrace W2",
+    href: "https://softmaps.io/map.php?startX=-3.072&startY=7.14&endX=-18.75&endY=7.46",
+  }, // Duplicate 7: Fixed
+  {
+    title: "R14 Portland Road W11 > Chilington Drive N7",
+    href: "https://softmaps.io/map.php?startX=-26.24&startY=6&endX=-18.42&endY=-6.124",
+  }, // Duplicate 6: Fixed
+  {
+    title: "R15 Cartwright Street E1 > Maygood Street N1",
+    href: "https://softmaps.io/map.php?startX=9.33&startY=6&endX=-1.19&endY=11.17",
+  },
+  {
+    title: "R16 Seagry Road E11 > Capworth Street E10",
+    href: "https://softmaps.io/map.php?startX=34.094&startY=18.02&endX=22.4&endY=17.34",
+  },
+  {
+    title: "R18 Armoury Way SW18 > Spencer Park SW18",
+    href: "https://softmaps.io/map.php?startX=-22.54&startY=-7.72&endX=-18.1&endY=-8.08",
+  },
+  {
+    title: "R20 Torrington Place WC1A > Margery Street WC1X",
+    href: "https://softmaps.io/map.php?startX=-6.25&startY=8.78&endX=-0.96&endY=9.73",
+  },
+  {
+    title: "R21 Prescot Street E1 > Brodlove Lane E1",
+    href: "https://softmaps.io/map.php?startX=9.69&startY=6.42&endX=15.33&endY=6.19",
+  },
+  {
+    title: "R24 Albion Street W2 > Frith Street W1D",
+    href: "https://softmaps.io/map.php?startX=-15.2&startY=6.93&endX=-6.315&endY=7.01",
+  },
+  {
+    title: "R25 Tufnell PK Rd. N7 > Elm Park Avenue N15",
+    href: "https://softmaps.io/map.php?startX=-5.21&startY=15.472&endX=10.27&endY=18.955",
+  },
+  {
+    title: "R26 Goodge Street W1T > St Pancras Road N1C",
+    href: "https://softmaps.io/map.php?startX=-7.76&startY=7.955&endX=-4.43&endY=10.47",
+  },
+  {
+    title: "R27 Half Moon Street W1J > Park Village East NW1",
+    href: "https://softmaps.io/map.php?startX=-9.59&startY=5.08&endX=-8.87&endY=10.72",
+  },
+  {
+    title: "R28 New Quebec Street W1H > Dean Street W1D",
+    href: "https://softmaps.io/map.php?startX=-13.05&startY=7.24&endX=-6.52&endY=6.99",
+  }, // Duplicate: Fixed
+  {
+    title: "R30 Blomfield Villas W2 > Noble Street EC2V",
+    href: "https://softmaps.io/map.php?startX=-20.13&startY=8.43&endX=3.12&endY=7.48",
+  },
+  {
+    title: "R31 Mark Lane EC3R > Heneage Street E1",
+    href: "https://softmaps.io/map.php?startX=6.90&startY=5.975&endX=9.45&endY=8",
+  },
+  {
+    title: "R32 Cumberland Market NW1 > Lower Belgrave Street SW1W",
+    href: "https://softmaps.io/map.php?startX=-9.17&startY=10.13&endX=-9.915&endY=2.58",
+  },
+  {
+    title: "R33 Maple Street W1T > Curzon Street W1J",
+    href: "https://softmaps.io/map.php?startX=-7.93&startY=8.7&endX=-10.63&endY=5.18",
+  }, // Duplicate: Fixed
+  {
+    title: "R34 Moscow Road W2 > Chamber Street E1",
+    href: "https://softmaps.io/map.php?startX=-21.9&startY=6.38&endX=9.27&endY=6.235",
+  },
+  {
+    title: "R35 Campden Street W8 > Cromer Street WC1H",
+    href: "https://softmaps.io/map.php?startX=-22.6&startY=5.22&endX=-4.08&endY=9.809",
+  },
+  {
+    title: "R36 Paul Street EC2A > Gray’s Inn Square WC1R",
+    href: "https://softmaps.io/map.php?startX=6.03&startY=9.42&endX=-1.415&endY=8.155",
+  },
+  {
+    title: "R37 Yeo Street E3 > Silk Weaver Way E2",
+    href: "https://softmaps.io/map.php?startX=22.99&startY=8.01&endX=13.26&endY=10.78",
+  },
+  {
+    title: "R43 Cavendish Mews South W1W > Collier Street N1",
+    href: "https://softmaps.io/map.php?startX=-9.2&startY=8.15&endX=-2.5&endY=10.78",
+  },
+  {
+    title: "R46 Baker Street EN5 > Cowley Hill WD6",
+    href: "https://softmaps.io/map.php?startX=-25.55&startY=29.83&endX=-41.02&endY=28.664",
+  },
+  {
+    title: "R49 Baythorne Street E3 > Southwark Park Road SE16",
+    href: "https://softmaps.io/map.php?startX=20.188&startY=7.88&endX=15.4&endY=0.8",
+  }, // 28
+]; // 88
